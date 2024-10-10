5f56484771bdbe786de3309d8ca23b3b7954ea33
davesheffda <davesheffda@gmail.com>
2024-10-10 15:28:59 +0100

    add functionality to find IP address and lat/longitude from ip address

--- src/get_data.py
+++ src/get_data.py
@@ -1,9 +1,19 @@
 import datetime
+from datetime import datetime
+from ip2geotools.databases.noncommercial import DbIpCity
+import socket
 
 def get_data():
-    time = datetime.datetime
+    time = f'{datetime.now()}'
     print(time)
+    hostname = socket.gethostname()
+    ip = socket.gethostbyname(hostname)
+    print(f'IP address is {ip}')
+    res = DbIpCity.get(ip, api_key="free")
+    latitude = res.latitude
+    longitude = res.longitude
+    print(f'Latitude -->> {latitude}, Longitude -->> {longitude} ')
     return time
-    # with open()
+    
 
 get_data()
