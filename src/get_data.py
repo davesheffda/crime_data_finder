80b3b3411ae56230fa39d9ded6b29c31d3ae071f
davesheffda <davesheffda@gmail.com>
2025-01-06 15:15:15 +0000

    amend commands to retrieve ip address

--- src/get_data.py
+++ src/get_data.py
@@ -1,20 +1,36 @@
-import datetime
 from datetime import datetime
-from ip2geotools.databases.noncommercial import DbIpCity
+import ipinfo
 import socket
+import os
+import sys
+from urllib.request import urlopen
+import re
+from dotenv import load_dotenv
 
-def get_data():
-    time = f'{datetime.now()}'
+load_dotenv()
+
+def getIP():
+    d = str(urlopen('http://checkip.dyndns.com/').read())
+    ip_address = re.compiler(r'Address: (\d+\.\d+|.|d+\.\d+)').search(d).group(1)
+    return
+
+def get_data(ip_address):
+    time = f"{datetime.now()}"
     time = time[:22]
-    print(f'The current date and time is {time}')
-    hostname = socket.gethostname()
-    ip = socket.gethostbyname(hostname)
-    print(f'IP address is {ip}')
-    res = DbIpCity.get(ip, api_key="free")
-    latitude = res.latitude
-    longitude = res.longitude
-    print(f'Latitude -->> {latitude}, Longitude -->> {longitude} ')
+    print(f"The current date and time is {time}")
+    # hostname = socket.gethostname()
+    # ip = socket.gethostbyname(hostname)
+    ip = ip_address
+    print(f"IP address is {ip}")
+    access_token = os.getenv('ACCESS_TOKEN')
+    handler = ipinfo.getHandler(access_token)
+    details = handler.getDetails(ip)
+    for key, value in details.all.items():
+       print(f"{key}: {value}") 
+    # latitude = res.latitude
+    # longitude = res.longitude
+    # print(f"Latitude -->> {latitude}, Longitude -->> {longitude} ")
     return time
-    
+
 
 get_data()
--- test/test_get_data.py
+++ test/test_get_data.py
@@ -1,13 +1,13 @@
 from src.get_data import get_data
-import re
+
 
 def test_function_creates_a_variable_containing_the_date_time():
     assert isinstance(get_data(), str)
 
+
 def test_date_and_time_stored_in_correct_format():
-    reg = re.compile('\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}\.\d{2}')
+    reg = re.compile("\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}\.\d{2}")
     time = get_data()
     found_time = reg.search(time)
-    print(found_time)
     assert found_time
-    
+
