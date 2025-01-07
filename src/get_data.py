6bf3edc251b14e2065616f4f170240bb75a19f9d
davesheffda <davesheffda@gmail.com>
2025-01-07 14:55:56 +0000

    revise IP retrieval and location functionality

--- src/get_data.py
+++ src/get_data.py
@@ -1,30 +1,22 @@
 from datetime import datetime
-import ipinfo
-import socket
 import os
-import sys
-from urllib.request import urlopen
-import re
+import ipinfo
+import requests
 from dotenv import load_dotenv
 
 load_dotenv()
 
-def getIP():
-    d = str(urlopen('http://checkip.dyndns.com/').read())
-    ip_address = re.compiler(r'Address: (\d+\.\d+|.|d+\.\d+)').search(d).group(1)
-    return
 
-def get_data(ip_address):
+def get_data():
     time = f"{datetime.now()}"
     time = time[:22]
     print(f"The current date and time is {time}")
-    # hostname = socket.gethostname()
-    # ip = socket.gethostbyname(hostname)
-    ip = ip_address
-    print(f"IP address is {ip}")
+    response = requests.get('https://api.ipify.org?format=json')
+    ip_address = response.json()['ip']
+    print(f"IP address is {ip_address}")
     access_token = os.getenv('ACCESS_TOKEN')
     handler = ipinfo.getHandler(access_token)
-    details = handler.getDetails(ip)
+    details = handler.getDetails(ip_address)
     for key, value in details.all.items():
        print(f"{key}: {value}") 
     # latitude = res.latitude
