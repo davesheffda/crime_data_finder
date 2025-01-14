eeab80827108feff3d70c932c0f00a7107d07509
davesheffda <davesheffda@gmail.com>
2025-01-14 15:23:59 +0000

    revised functionality to return list of containing time, latitude and longitude. Refactor current tests to reflect new funcionality

--- src/get_data.py
+++ src/get_data.py
@@ -9,20 +9,18 @@ load_dotenv()
 
 def get_data():
     time = f"{datetime.now()}"
-    time = time[:22]
+    time = time[:19]
     print(f"The current date and time is {time}")
-    response = requests.get('https://api.ipify.org?format=json')
-    ip_address = response.json()['ip']
-    print(f"IP address is {ip_address}")
-    access_token = os.getenv('ACCESS_TOKEN')
+    response = requests.get("https://api.ipify.org?format=json")
+    ip_address = response.json()["ip"]
+    print(f"Your IP address is {ip_address}")
+    access_token = os.getenv("ACCESS_TOKEN")
     handler = ipinfo.getHandler(access_token)
     details = handler.getDetails(ip_address)
-    for key, value in details.all.items():
-       print(f"{key}: {value}") 
-    # latitude = res.latitude
-    # longitude = res.longitude
-    # print(f"Latitude -->> {latitude}, Longitude -->> {longitude} ")
-    return time
+    latitude = details.latitude
+    longitude = details.longitude
+    print(f"Your latitude is -->> {latitude}, Your longitude is -->> {longitude} ")
+    return [time, latitude, longitude]
 
 
 get_data()
--- test/test_get_data.py
+++ test/test_get_data.py
@@ -1,13 +1,23 @@
 from src.get_data import get_data
+import re
 
 
-def test_function_creates_a_variable_containing_the_date_time():
-    assert isinstance(get_data(), str)
+def test_function_creates_a_list_containing_the_date_time_as_the_first_element():
+    returned_data = get_data()
+    assert isinstance(returned_data[0], str)
 
 
 def test_date_and_time_stored_in_correct_format():
-    reg = re.compile("\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}\.\d{2}")
-    time = get_data()
-    found_time = reg.search(time)
+    reg = re.compile(r"\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}\.\d{2}")
+    returned_data = get_data()
+    found_time = reg.search(returned_data[0])
     assert found_time
+
+
+def test_latitude_and_longitude_returned():
+    returned_data = get_data()
+    assert isinstance(returned_data, list)
+    assert len(returned_data) == 3
+
+# def test_latitude_and_longitude_are_in_correct_format():
 
