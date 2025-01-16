caaf06a577e276275929c36d5246ec3a843acbec
davesheffda <davesheffda@gmail.com>
2025-01-16 09:56:22 +0000

    further refactor get_time_string function

--- src/get_time.py
+++ src/get_time.py
@@ -4,4 +4,7 @@ def get_time_string():
     time = f"{datetime.now()}"
     time = time[:19]
     time_string = f"The current date and time is {time}"
+    print(time_string)
     return time_string
+
+
