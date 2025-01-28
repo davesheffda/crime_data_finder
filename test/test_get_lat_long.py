483a959b3015fedc16120bd457e73da4b07beefd
davesheffda <davesheffda@gmail.com>
2025-01-28 18:59:43 +0000

    amend test for get_lat_long now dict includes IP address

--- test/test_get_lat_long.py
+++ test/test_get_lat_long.py
@@ -3,7 +3,7 @@ from src.get_lat_long import get_lat_long
 def test_latitude_and_longitude_returned_as_dictionary():
     returned_data = get_lat_long()
     assert isinstance(returned_data, dict)
-    assert len(returned_data) == 2
+    assert len(returned_data) == 3
 
 def test_latitude_and_longitude_are_in_correct_format():
     lat_long = get_lat_long()
