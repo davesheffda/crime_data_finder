b03c10c97a9827249b252813cc51ed0f26202d6b
davesheffda <davesheffda@gmail.com>
2025-01-29 21:51:45 +0000

    amend get_force function doc string

--- src/get_force.py
+++ src/get_force.py
@@ -3,7 +3,7 @@ from src.get_lat_long import get_lat_long
 
 def get_force():
     '''
-    Function that uses the police API to retrieve the unique force code and force specific team identifier for a british police base on the latitute and longitude
+    Function that uses the police API and get_lat_long function to retrieve the unique force code and force specific team identifier for a british police base on the latitute and longitude
     '''
     lat_long = get_lat_long()
     response = requests.get(f"https://data.police.uk/api/locate-neighbourhood?q={lat_long['lat']},{lat_long['long']}")
