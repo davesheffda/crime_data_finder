4d83cda1016018dab5d3fa7fcae059062dc3b857
davesheffda <davesheffda@gmail.com>
2025-01-30 09:12:54 +0000

    add draft request code for street level crimes to get_data function

--- src/get_data.py
+++ src/get_data.py
@@ -1,6 +1,7 @@
 from src.get_time import get_time_string
 from src.get_lat_long import get_lat_long
 from src.get_force import get_force
+import requests
 
 
 import requests
@@ -12,6 +13,12 @@ def get_data():
     print(f"Your IP address is {lat_long_ip['ip']}")
     print(f"Your latitude is -->> {lat_long_ip['lat']}, Your longitude is -->> {lat_long_ip['long']} ")
     get_force()
+    latitude = lat_long_ip['lat']
+    longitude = lat_long_ip['long']
+    response = requests.get(f"https://data.police.uk/api/crimes-street/all-crime?{latitude}&{[longitude]}")
+    crimes = response.json()
+    print(crimes)
+
     return
 
 
