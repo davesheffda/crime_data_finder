af700b2103fd45dd708312857dbb11194a71a854
davesheffda <davesheffda@gmail.com>
2025-02-04 12:22:08 +0000

    add function get_last_update that returns string with date of last police API data update.

---
+++ src/get_last_update.py
@@ -0,0 +1,14 @@
+
+
+def get_last_update():
+    '''
+    Checks the Police API and returns date when the API was last updated
+
+    Args: None
+    Returns: Date in string format 
+    Examples:
+        get_last_update()
+        # "The last time the Police API was update was 2024-11-01"
+    '''
+    last_update = ''
+    return last_update
