97eed9118b764fb5fd15eb764f734bb278b08116
davesheffda <davesheffda@gmail.com>
2025-02-06 10:57:46 +0000

    add two tests for revised dictionary output of get_last_update function

--- test/test_get_last_update.py
+++ test/test_get_last_update.py
@@ -1,4 +1,12 @@
 from src.get_last_update import get_last_update
 
-def test_function_returns_string():
-    assert isinstance(get_last_update(), str)
+def test_function_returns_dict():
+    assert isinstance(get_last_update(), dict)
+
+def test_function_returns_dictionary_with_date_key():
+    test_date = get_last_update()
+    assert 'date' in test_date
+
+def test_function_returns_dictionary_with_string_in_date_key():
+    test_date = get_last_update()
+    assert isinstance(test_date['date'], str)
