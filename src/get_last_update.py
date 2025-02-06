2c5e0aa044356d7309f4a75889edb4b2870df153
davesheffda <davesheffda@gmail.com>
2025-02-06 10:58:25 +0000

    amend get_last_udpate function to return dictionary and print string with date in.

--- src/get_last_update.py
+++ src/get_last_update.py
@@ -8,11 +8,12 @@ def get_last_update():
     Returns: Date in string format 
     Examples:
         get_last_update()
-        # "The last time the Police API was update was 2024-11-01"
+        # "The police API data was last updated on 2024-11-01"
     '''
     response = requests.get("https://data.police.uk/api/crime-last-updated")
-    print(response.json())
-    last_update = response.dumps()
+    last_update = response.json()
+    last_update_date = last_update['date']
+    print(f'The police API data was last updated on {last_update_date}')
     return last_update
 
 get_last_update()
